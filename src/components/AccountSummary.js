f40c1cc394360778e01f87671dd9dc8a8723bd14
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-06-16 12:24:21 +0500

    CSS updates and AccountSummary updates
    Removed signs from summary amounts and updated css

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -17,20 +17,20 @@ export const AccountSummary = () => {
     const expense = Math.abs(transactionAmounts
         .filter(transaction => transaction < 0)
         .reduce((acc, transaction) => (acc += transaction), 0)
-        .toFixed(2));
+        ).toFixed(2);
 
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
                 <p className="money plus">
-                    +{income}
+                    {income}
                 </p>
             </div>
             <div>
                 <h4>Expense</h4> 
                 <p className="money minus">
-                    -{expense}
+                    {expense}
                 </p>
             </div>
             
