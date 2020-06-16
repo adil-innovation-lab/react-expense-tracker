892dfd92e0e8e8633f6b3250bddd7986843f79ab
adil-innovation-lab <khanadilaltaf@gmail.com>
2020-06-16 08:54:11 +0500

    Created State for Adding New Transactions
    Updated the AddTransaction.js file to use the useState hook to create state so we can add new transactions with the form.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const AddTransaction = () => {
+
+    const [description, setDescription] = useState('');
+    const [transactionAmount, setTransactionAmount] = useState(0);
+
     return (
         <div>
             <h3>Add New Transaction</h3>
@@ -9,13 +13,23 @@ export const AddTransaction = () => {
                     <label htmlFor="description">
                         Description
                     </label>
-                    <input type="text" id="description" placeholder="Detail of Transaction" />
+                    <input  type="text" 
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Detail of Transaction" 
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="transactionamount">
                         Transaction Amount
                     </label>
-                    <input type="number" id="transactionamount" placeholder="Dollar Value of Transaction" />
+                    <input  type="number" 
+                            id="transactionamount"
+                            value={transactionAmount}
+                            onChange={(e) => setTransactionAmount(e.target.value)}
+                            placeholder="Dollar Value of Transaction" 
+                    />
                 </div>
                 <button className="btn">Add Transaction</button>
             </form>
